edb4b77268bd891dfd1943f597f9bf66742c1196
aryansingh9909 <aryansingh9909@gmail.com>
2020-09-16 09:18:27 +0530

    Adding files

---
+++ src/main/java/com/saket/SaketApplication.java
@@ -0,0 +1,13 @@
+package com.saket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saket/SaketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
